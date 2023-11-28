a8f052c976a3ecc0f80f70438b0a74818830633f
Chuxiness <146590179+Chuxiness@users.noreply.github.com>
2023-11-28 09:11:12 +0000

    fully functional

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,4 +9,14 @@ class DirectorsController < ApplicationController
 
     render({ :template => "directors_template/details"})
   end
+  def young
+    not_null_director = Director.where.not({ :dob => nil })
+    @youngest = not_null_director.order(dob: :desc).first
+    render({ :template => "age_templates/youngest"})
+  end
+  def old
+    not_null_director = Director.where.not({ :dob => nil })
+    @oldest = not_null_director.order(dob: :asc).first
+    render({ :template => "age_templates/oldest"})
+  end
 end
